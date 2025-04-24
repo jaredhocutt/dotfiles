ebdc7ce554d3751227c2b4ae1430a9f683c1e391
Jared Hocutt <jaredhocutt@gmail.com>
2025-04-24 14:48:57 -0400

    Configure macOS dock
    Signed-off-by: Jared Hocutt <jaredhocutt@gmail.com>

---
+++ .chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+set -eufo pipefail
+
+trap 'killall Dock' EXIT
+
+declare -a remove_labels=(
+	Safari
+	Maps
+	FaceTime
+	Calendar
+	Contacts
+	Freeform
+	TV
+	Music
+    News
+	Keynote
+	Numbers
+	Pages
+	"App Store"
+    "Managed Software Center"
+    "Red Hat Printer Setup"
+    "Red Hat Mac Info"
+)
+
+for label in "${remove_labels[@]}"; do
+    if dockutil --find ${label} >/dev/null 2>&1; then
+        echo "Found ${label} in Dock, removing it..."
+        dockutil --no-restart --remove "${label}" || true
+    fi
+done
