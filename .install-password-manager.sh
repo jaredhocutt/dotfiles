ed911d7f29f0c3480841f17cb028b764156b9040
Jared Hocutt <jaredhocutt@gmail.com>
2026-09-04 14:25:47 -0400

    Improve bitwarden handling
    Signed-off-by: Jared Hocutt <jaredhocutt@gmail.com>

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -2,45 +2,39 @@
 
 set -euo pipefail
 
-# Exit this script if the Bitwarden CLI is already installed
-type bw >/dev/null 2>&1 && exit
+if ! type bw >/dev/null 2>&1; then
+    echo "Installing Bitwarden CLI..."
 
-echo "Installing Bitwarden CLI..."
-
-case "$(uname -s)" in
-Darwin)
-    /opt/homebrew/bin/brew install bitwarden-cli
-    ;;
-Linux)
-    if ! command -v unzip >/dev/null 2>&1; then
-        if command -v apt >/dev/null 2>&1; then
-            sudo apt install -y unzip
-        elif command -v dnf >/dev/null 2>&1; then
-            sudo dnf install -y unzip
+    case "$(uname -s)" in
+    Darwin)
+        /opt/homebrew/bin/brew install bitwarden-cli
+        ;;
+    Linux)
+        if ! command -v unzip >/dev/null 2>&1; then
+            if command -v apt >/dev/null 2>&1; then
+                sudo apt install -y unzip
+            elif command -v dnf >/dev/null 2>&1; then
+                sudo dnf install -y unzip
+            fi
         fi
-    fi
 
-    TMPDIR="$(mktemp -d)"
+        TMPDIR="$(mktemp -d)"
 
-    curl -fsSL "https://bitwarden.com/download/?app=cli&platform=linux" -o "${TMPDIR}/bw.zip"
-    unzip -qo "${TMPDIR}/bw.zip" -d "${TMPDIR}"
+        curl -fsSL "https://bitwarden.com/download/?app=cli&platform=linux" -o "${TMPDIR}/bw.zip"
+        unzip -qo "${TMPDIR}/bw.zip" -d "${TMPDIR}"
 
-    sudo install -m 0755 "${TMPDIR}/bw" "/usr/local/bin/bw"
-    ;;
-*)
-    echo "Unsupported OS"
-    exit 1
-    ;;
-esac
+        sudo install -m 0755 "${TMPDIR}/bw" "/usr/local/bin/bw"
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
+    esac
 
-echo "Bitwarden CLI installed successfully"
+    echo "Bitwarden CLI installed successfully"
+fi
 
-echo "Logging in to Bitwarden CLI..."
-
-# Perform the initial login to Bitwarden
-bw login
-
-echo "Unlocking Bitwarden and exporting session key as BW_SESSION..." >&2
-local session
-session=$(bw unlock --raw) || { echo "bw unlock failed" >&2; return 1; }
-export BW_SESSION="$session"
+if ! bw login --check >/dev/null 2>&1; then
+    echo "Logging in to Bitwarden CLI..."
+    bw login
+fi
